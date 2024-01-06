44ff81b76120048fddd4486f54d99d8f8f158662
Carlos German <carlosgerman353@gmail.com>
2024-01-06 16:37:25 -0500

    Removes first argument being passed when it's a number
    Having issues with passing all other tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, remove1, remove2) {
+    const arrRemoved = [];
 
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i] != remove1) {
+            arrRemoved.push(arr[i]);
+        }
+    }
+    return arrRemoved;
 };
+
+console.log(removeFromArray([1,2,3], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
