cd888e3583c3d285043627e59c31ba3085b5a529
Carlos German <carlosgerman353@gmail.com>
2024-01-06 17:07:46 -0500

    Pass first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    const numbersToAdd = []
+
+    for(let i = num1; i <= num2; i++) {
+        numbersToAdd.push(i);
+    }
+
+    for(j = 0; j < numbersToAdd.length; j++) {
+        sum += numbersToAdd[j];
+    }
+
+    return sum; 
 
 };
+
+console.log(sumAll(1, 4)); 
 
 // Do not edit below this line
 module.exports = sumAll;
