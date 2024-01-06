81bf1dd553a7d54fcd034dd0cbe7982925c49a9c
Carlos German <carlosgerman353@gmail.com>
2024-01-06 11:31:27 -0500

    Solve problem 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let wordReversed = "";
 
+    for(let i = word.length - 1; i >= 0; i--) {
+        wordReversed += word[i];
+    }
+
+    return wordReversed;
+    
 };
+
+console.log(reverseString("things"));
 
 // Do not edit below this line
 module.exports = reverseString;
