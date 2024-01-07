e2daa5ce94edc07e096db165d98a698c99bf7b1b
Carlos German <carlosgerman353@gmail.com>
2024-01-07 14:54:09 -0500

    Solve problem 4
    need help, had to peak at the solution. Learned a few things like using rest operator.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,28 @@
-const removeFromArray = function(arr, remove1, remove2) {
+// const removeFromArray = function(arr, remove1, remove2) {
+//     const arrRemoved = [];
+
+//     for(let i = 0; i < arr.length; i++) {
+//         if (arr[i] != remove1) {
+//             arrRemoved.push(arr[i]);
+//         }
+//     }
+//     return arrRemoved;
+// };
+
+const removeFromArray = function(array, ...argus) {
     const arrRemoved = [];
 
-    for(let i = 0; i < arr.length; i++) {
-        if (arr[i] != remove1) {
-            arrRemoved.push(arr[i]);
+    array.forEach((item) => {
+        if (!argus.includes(item)) {
+            arrRemoved.push(item);
         }
-    }
+    });
     return arrRemoved;
 };
 
-console.log(removeFromArray([1,2,3], 3));
+
+
+console.log(removeFromArray([1,2,3], 3,2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
