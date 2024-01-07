6a1f2653345c25f758dc0abbeabd84665309dd08
Carlos German <carlosgerman353@gmail.com>
2024-01-07 15:05:04 -0500

    Works with large numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,8 +2,18 @@ const sumAll = function(num1, num2) {
     let sum = 0;
     const numbersToAdd = []
 
-    for(let i = num1; i <= num2; i++) {
-        numbersToAdd.push(i);
+    if (num1 < num2) {
+        for(let i = num1; i <= num2; i++) {
+            numbersToAdd.push(i);
+        }
+    }
+    else if (num1 > num2) {
+        let newNum1 = num2;
+        let newNum2 = num1; 
+
+        for(let i = newNum1; i <= newNum2; i++) {
+            numbersToAdd.push(i);
+        }
     }
 
     for(j = 0; j < numbersToAdd.length; j++) {
@@ -15,6 +25,7 @@ const sumAll = function(num1, num2) {
 };
 
 console.log(sumAll(1, 4)); 
+console.log(sumAll(123, 1)); 
 
 // Do not edit below this line
 module.exports = sumAll;
