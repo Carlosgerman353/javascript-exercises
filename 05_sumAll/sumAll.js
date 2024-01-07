19052eb9e9928bce333fe34f5982c8d057097200
Carlos German <carlosgerman353@gmail.com>
2024-01-07 15:10:09 -0500

    Returns ERROR with negative numbers as it should.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,6 +2,12 @@ const sumAll = function(num1, num2) {
     let sum = 0;
     const numbersToAdd = []
 
+    if ( (num1 < 0 || num2 < 0) || 
+        (typeof num1 === "string" || typeof num2 === "string") 
+        ) {
+        return "ERROR";
+    }
+
     if (num1 < num2) {
         for(let i = num1; i <= num2; i++) {
             numbersToAdd.push(i);
